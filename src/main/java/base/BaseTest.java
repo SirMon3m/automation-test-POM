ec5c1f1decdf3f1e6b24f5b1ef0d9449626587c7
svare <mon3mpk@gmail.com>
2025-04-20 22:45:38 +0200

    base page done

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,4 +1,36 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+
+    protected WebDriver driver  ;
+
+
+    @BeforeMethod
+    public void setup(){
+
+        driver = new ChromeDriver(); //launches new Chrome window
+
+        driver.manage().window().maximize(); //maximizing the browser window
+
+        driver.get("https://www.bmw-egypt.com/en/configure.html"); // the tested website url
+
+
+
+    }
+    @AfterMethod
+
+    public void tearDown(){
+
+        if(driver != null){
+
+            driver.quit();
+        }
+    }
+
+
 }
