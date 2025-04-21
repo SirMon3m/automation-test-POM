a32fedd7621b57be232bbf6bfdf64db443426ce4
svare <mon3mpk@gmail.com>
2025-04-21 12:55:23 +0200

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,8 +1,18 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
 
-    
+
+
+    private WebDriver driver ; //private because we dont want to use it anywhere else
+
+
+    private By usernameLogin = By.id("user"); //accessing the username field
+    private By passwordLogin = By.id("pass"); //accessing the password field
+    private By buttonLogin = By.id("button"); //accessing the login button
 
 
 }
